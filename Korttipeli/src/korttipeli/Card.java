cf11c652c8cf3a160b60346090599b549c506181
Z1M1 <31651454+Z1M1@users.noreply.github.com>
2017-11-14 09:01:31 +0200

    Add files via upload

---
+++ Korttipeli/src/korttipeli/Card.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package korttipeli;
+
+/**
+ *
+ * @author s1601375
+ */
+public class Card {
+    private Suit suit;
+	private Value value;
+	
+	public Card(Suit suit, Value value){
+		this.suit = suit;
+		this.value = value;
+	}
+	
+	public String toString(){
+		return this.suit.toString() + "-" + this.value.toString();
+	}
+	
+	public Value getValue(){
+		return this.value;
+	}
+
+
+	
+}
+
+
---
+++ Korttipeli/src/korttipeli/Suit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package korttipeli;
+
+/**
+ *
+ * @author s1601375
+ */
+public class Suit {
+  public enum suit {
+	HEART, SPADE, DIAMOND, CLUB
+  }}
+  
+
